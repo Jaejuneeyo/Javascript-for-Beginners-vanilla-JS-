5d617a64ccfc0e6779dee25ff3f56748641fdaf2
sin jae joon <grandfinal12@likelion.org>
2020-08-22 22:07:23 +0900

    color change

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ wrapper = document.querySelector('.wrapper');
 
 function _callback() {
     const color = '#'+ parseInt(Math.round(Math.random()*0xFFFFFF)).toString(16);
-    wrapper.sytle.backgroundColor= color;
+    wrapper.style.backgroundColor= color;
 }
 wrapper.addEventListener('click', _callback)
